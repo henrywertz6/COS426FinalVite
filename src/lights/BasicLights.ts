821cbaf48b1e7c4b79ecb92638af2bec4d69e79c
henrywertz6 <90224775+henrywertz6@users.noreply.github.com>
2023-12-11 19:37:21 -0500

    fixed the lighting so you can see the boat

--- src/lights/BasicLights.ts
+++ src/lights/BasicLights.ts
@@ -1,21 +1,33 @@
-import { Group, SpotLight, AmbientLight, HemisphereLight } from 'three';
+import {
+    Group,
+    SpotLight,
+    AmbientLight,
+    HemisphereLight,
+    DirectionalLight,
+} from 'three';
 
 class BasicLights extends Group {
     constructor() {
         // Invoke parent Group() constructor
         super();
 
-        const dir = new SpotLight(0xffffff, 1.6, 7, 0.8, 1, 1);
+        // const dir = new SpotLight(0xffffff, 1.6, 15, 0.8, 1, 1);
         const ambi = new AmbientLight(0x404040, 5);
-        const ambi2 = new AmbientLight(0x404040, 5);
-        const ambi3 = new AmbientLight(0x404040, 5);
-        const amb4 = new AmbientLight(0x404040, 5);
         const hemi = new HemisphereLight(0xffffbb, 0x080820, 2.3);
 
-        dir.position.set(10, 2, 5);
-        dir.target.position.set(0, 0, 0);
+        // dir.position.set(10, 2, 5);
+        ambi.position.set(10, 2, 5);
+        // dir.target.position.set(0, 0, 0);
 
-        this.add(ambi, ambi2, ambi3, amb4, hemi, dir);
+        let hemiLight = new HemisphereLight(0xffffff, 0x444444);
+        hemiLight.position.set(0, 300, 0);
+
+        let dirLight = new DirectionalLight(0xffffff, 3);
+        dirLight.position.set(75, 300, -75);
+        let dirLight2 = new DirectionalLight(0xffffff, 3);
+        dirLight.position.set(75, -300, -75);
+
+        this.add(ambi, hemi, dirLight, hemiLight, dirLight2);
     }
 }
 
